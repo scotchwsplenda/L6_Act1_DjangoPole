3babafca099bd550af499c1fd0f7243652bdce1d
Oliver Lockwood (CBRE) <v-ollock@microsoft.com>
2020-11-13 10:47:48 -0800

    Django Views

--- obl_site/obl_site/urls.py
+++ obl_site/obl_site/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('blogging.urls')),
     path('admin/', admin.site.urls),
     path('polling/', include('polling.urls'))
 ]
